c6bbd026815b7984d3b50eb9ba77936edc38acfb
root <Francoherrera123@francoh261@gmail.com>
2023-07-07 17:43:05 +0000

    Simple console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Console """
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit on EOF (Ctrl+D)"""
+        print()  # Print a newline before exiting
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
