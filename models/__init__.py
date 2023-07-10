bac0ab951512bb26274ccfe56beba7eaf59d2ffa
Felipe Pereira Forte <felipereiraforte@gmail.com>
2023-07-09 17:31:37 -0700

    init fix

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,8 @@
 
 from models.engine.file_storage import FileStorage
 
+valid_classes = {'BaseModel': 'BaseModel', 'User': 'User',
+                 'City': 'City', 'Place': 'Place', 'Review': 'Review',
+                 'Amenity': 'Amenity', 'State': 'State'}
 storage = FileStorage()
 storage.reload()
